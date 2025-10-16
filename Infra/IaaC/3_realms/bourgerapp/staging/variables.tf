30ec56260a51d88e8ba1119daf09882858b27c35
fgajic <filipgajic.gajic1@gmail.com>
2025-10-16 10:35:27 +0300

    restructuring

--- Infra/IaaC/3_realms/bourgerapp/staging/variables.tf
+++ Infra/IaaC/3_realms/bourgerapp/staging/variables.tf
@@ -3,7 +3,7 @@ variable "common_tags" {
   type        = map(string)
   default = {
     environment = "staging-test"
-    owner       = "Filip Gajic"
+    owner       = "Filip"
     project     = "burgerapp"
   }
 }
